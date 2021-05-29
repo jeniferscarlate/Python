b1f238020cbef17a0a7b675d37c49fb6c5efc665
JeniferScarlate <72558032+jeniferscarlate@users.noreply.github.com>
2021-05-29 00:34:53 -0300

    commit

---
+++ ex002.py
@@ -0,0 +1,4 @@
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite mais um número: '))
+soma = (num1 + num2)
+print('A soma entre {} e {} é igual a {}'.format(num1, num2, soma))
---
+++ ex003.py
@@ -0,0 +1,11 @@
+algo = input('Digite algo: ')
+print('O tipo primitivo desse valor é {}'.format(type(algo)))
+print('É alfanumérico?', algo.isalnum())
+print('É alfabético?', algo.isalpha())
+print('Está em minúsculo?', algo.islower())
+print('Está em maiúsculo?', algo.isupper())
+print('Só tem espaços?', algo.isspace())
+print('Está capitalizada?', algo.istitle())
+
+
+
