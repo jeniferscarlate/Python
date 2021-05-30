0048031ea9ff52abc423794893cd6c2ff2496223
JeniferScarlate <72558032+jeniferscarlate@users.noreply.github.com>
2021-05-30 15:04:36 -0300

    Update ex006.py

--- ex006.py
+++ ex006.py
@@ -4,8 +4,8 @@
 num = int(input('Digite um número: '))
 dobro = num * 2
 triplo = num * 3
-raizq = num **(1/2)
+raizq = num ** (1/2)
 
 print('O dobro de {} é igual a {}.'.format(num, dobro))
 print('O triplo de {} é igual a {}.'.format(num, triplo))
-print('A raiz quadrada de {} é igual a {}.'.format(num, raizq))
+print('A raiz quadrada de {} é igual a {:.2f}.'.format(num, raizq))
