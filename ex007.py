ca740c59ff44701eda9b8f8f3a89bfe893e663fd
JeniferScarlate <72558032+jeniferscarlate@users.noreply.github.com>
2021-05-30 15:08:31 -0300

    Update ex007.py

--- ex007.py
+++ ex007.py
@@ -3,5 +3,5 @@
 
 nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
-media = nota1 / nota2
-print('A média das notas é {}'.format(media))
+media = (nota1 + nota2)/2
+print('A média das notas é {:.1f}'.format(media))
