67124b4b50fd4e1f5713ae892c326eecded4c1ce
JeniferScarlate <72558032+jeniferscarlate@users.noreply.github.com>
2021-06-01 00:19:27 -0300

    Update ex014.py

--- ex014.py
+++ ex014.py
@@ -1,8 +1,8 @@
 # Escreva uma programa que converta uma temperatura digitada em ºC para ºF.
 
-print('-'*50)
-tempC = float(input('Digite a temperatrura em graus celsius: '))
+print('-'*45)
+tempC = float(input('Digite a temperatrura em ºC : '))
 tempF = (tempC * 1.8)+32
 
 print('A temperatura de {}ºC equivale a {}ºF'.format(tempC, tempF))
-print('-'*50)
+print('-'*45)
