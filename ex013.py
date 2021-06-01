caec60122a7ac6f2608db4a9d537accbb5a40c5b
JeniferScarlate <72558032+jeniferscarlate@users.noreply.github.com>
2021-06-01 00:04:47 -0300

    Update ex013.py

--- ex013.py
+++ ex013.py
@@ -1,9 +1,10 @@
 # Faça um algoritmo que leia o salário de um funcionário e mostre seu
 # novo salário com 15% de aumento.
 
+print('-'*50)
 salario = float(input('Digite o salário do funcionário: R$'))
 novoSalario = salario + (salario*15/100)
 
-print('-'*50)
+
 print('O salário de R${:.2f}, com o aumento de 15%,\npassa a ser R${:.2f}.'.format(salario, novoSalario))
 print('-'*50)
